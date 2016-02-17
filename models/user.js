7d1b20e1ae1fc3c1bd13ae92c44ee290a85b6373
Simon <simon160@gmail.com>
2016-02-16 21:47:40 -0500

    added more css3 animations

---
+++ models/user.js
@@ -0,0 +1,27 @@
+
+var r = require("rethinkdbdash")();
+require("rethink-config")({
+  "r": r,
+  "database": "testDB",
+  "tables": ["PilotUsers"]
+});
+
+
+
+module.exports = r.model('User')
+
+
+
+
+
+/*
+
+// get an instance of mongoose and mongoose.Schema
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+// set up a mongoose model and pass it using module.exports
+module.exports = mongoose.model('User', new Schema({
+    name: String,
+    password: String,
+    grantType: String
+})); */
